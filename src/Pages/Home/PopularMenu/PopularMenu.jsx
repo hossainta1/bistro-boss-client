c01e8c1a212e06d0d22ab054eccf6a157f47301a
hossainta1 <tanvir.hossain303030@gmail.com>
2023-11-27 20:26:33 +0600

    View popular menu button

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -30,6 +30,9 @@ const PopularMenu = () => {
                     ></MenuItem>)
                 }
             </div>
+            <div className="text-center">
+                <button className="btn btn-outline border-0 border-b-4 mt-3 text-orange-400">View Full Menu</button>
+            </div>
         </section>
     );
 };
