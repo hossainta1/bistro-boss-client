1b7a4d16cccb1dbe1b5ba1e1c854a736f76fc3f0
hossainta1 <tanvir.hossain303030@gmail.com>
2023-11-28 20:30:17 +0600

    Impliment Dynamic page title using React Halmet

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,11 +3,16 @@ import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import Testimonials from "../Testimonials/Testimonials";
+import { Helmet } from 'react-helmet-async';
 
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro | Home</title>
+
+            </Helmet>
             <Banner></Banner>
             <Category></Category>
             <PopularMenu></PopularMenu>
---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet-async';
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro | Menu</title>
+
+            </Helmet>
+            <h2>This is Menu</h2>
+        </div>
+    );
+};
+
+export default Menu;
