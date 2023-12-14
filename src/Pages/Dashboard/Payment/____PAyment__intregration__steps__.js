082c4aeffcc5253b5217455b9ae7d5685af6adab
hossainta1 <tanvir.hossain303030@gmail.com>
2023-12-14 20:30:26 +0600

    create payment method

--- src/Pages/Dashboard/Payment/____PAyment__intregration__steps__.js
+++ src/Pages/Dashboard/Payment/____PAyment__intregration__steps__.js
@@ -3,7 +3,14 @@
  * 2. Create checkOutFrom with card element
  * 3. Create account on stripe 
  * 4. Get publisable key from stripe
- * 5. 
+ * 5. get card information
+ * 6. Create a payment method
+ * 7. use test card to test payment
+ * 8. On the server side install stripe > npm install --save stripe
+ * 9. Create a payment intent api on server side with payment method type  payment_method_types: ['card'] 
+ * 
+ * 11. MAke sure amount bust be cents(multiply price by 100)
+ * 
  * 
  * 
  * 
